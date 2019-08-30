bae64d4afe3e27a4fdb32b25fa33efcbadd45889
Andrzej Torski <andrew.torski@gmail.com>
2019-08-30 13:36:27 +0200

    equals, add and minus for tuples

--- src/main/kotlin/nd/rw/kray/Tuple.kt
+++ src/main/kotlin/nd/rw/kray/Tuple.kt
@@ -21,4 +21,33 @@ class Tuple(val x: Double, val y: Double, val z: Double, val w: Double) {
     val isVector: Boolean
         get() = w == 0.0
 
+    fun plus(other: Tuple): Tuple {
+        return Tuple(
+            x = this.x + other.x,
+            y = this.y + other.y,
+            z = this.z + other.z,
+            w = this.w + other.w
+        )
+    }
+
+    fun minus(other: Tuple): Tuple {
+        return Tuple(
+            x = this.x - other.x,
+            y = this.y - other.y,
+            z = this.z - other.z,
+            w = this.w - other.w
+        )
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other?.javaClass != this.javaClass) return false
+
+        other as Tuple
+
+        return this.x == other.x
+                && this.y == other.y
+                && this.z == other.z
+                && this.w == other.w
+    }
 }
--- src/test/groovy/nd/rw/kray/TupleUT.groovy
+++ src/test/groovy/nd/rw/kray/TupleUT.groovy
@@ -2,15 +2,65 @@ package nd.rw.kray
 
 import spock.lang.Specification
 
+import static nd.rw.kray.Tuple.point
+import static nd.rw.kray.Tuple.vector
+
 class TupleUT extends Specification {
 
-    def 'should create point and tuple'() {
+    def 'should create tuple as a point'() {
         given:
-            def point = Tuple.point(4.3, 4.2, 3.1)
+            def point = point(4.3, 4.2, 3.1)
 
         expect:
             point.isPoint()
             !point.isVector()
     }
 
+    def 'should create tuple as a vector'() {
+        given:
+            def vector = vector(1, 2, 3)
+
+        expect:
+            vector.isVector()
+            !vector.isPoint()
+    }
+
+    def 'should test correctly for equality and inequality'() {
+        given:
+            def vector1 = vector(1.1, 2, 3)
+            def vector2 = vector(4, 5, 6)
+            def vector3 = vector(1.1, 2, 3)
+
+        expect:
+            vector1 != vector2
+            vector1 == vector3
+    }
+
+    def 'adding two tuples'() {
+        given:
+            def tuple1 = new Tuple(3, -2, 5, 1)
+            def tuple2 = new Tuple(-2, 3, 1, 0)
+
+
+        expect:
+            tuple1 + tuple2 == new Tuple(1, 1, 6, 1)
+    }
+
+    def 'subtracting two points should result in a vector'() {
+        given:
+            def p1 = point(3, 2, 1)
+            def p2 = point(5, 6, 7)
+
+        expect:
+            p1 - p2 == vector(-2, -4, -6)
+    }
+
+    def 'subtracting a vector from a point should result in a point'() {
+        given:
+            def p = point(3, 2, 1)
+            def v = vector(5, 6, 7)
+
+        expect:
+            p - v == point(-2, -4, -6)
+    }
 }
