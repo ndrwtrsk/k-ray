70ba8eadf3a3adebc22616677dc70b8d516a2e53
Andrzej Torski <andrew.torski@gmail.com>
2019-10-30 17:07:44 +0100

    Add position function to ray

--- src/main/kotlin/nd/rw/kray/Ray.kt
+++ src/main/kotlin/nd/rw/kray/Ray.kt
@@ -2,4 +2,9 @@ package nd.rw.kray
 
 class Ray (val origin: Tuple, val direction: Tuple){
 
+    fun position(t: Number): Tuple {
+        return origin + direction * t
+    }
+
+
 }
--- src/test/kotlin/nd/rw/kray/RaySpec.kt
+++ src/test/kotlin/nd/rw/kray/RaySpec.kt
@@ -9,14 +9,34 @@ class RaySpec : Spek({
 
     describe("creating a ray") {
 
-        val origin = point(1,2,3)
-        val direction = vector(4,5,6)
+        val origin = point(1, 2, 3)
+        val direction = vector(4, 5, 6)
 
         val ray = Ray(origin, direction)
 
         it("querying ray") {
-            ray.origin.should.equal(point(1,2,3))
-            ray.direction.should.equal(vector(4,5,6))
+            ray.origin.should.equal(point(1, 2, 3))
+            ray.direction.should.equal(vector(4, 5, 6))
+        }
+    }
+
+    describe("computing a point from a distance") {
+        val ray = Ray(point(2, 3, 4), vector(1, 0, 0))
+
+        it("position at 0") {
+            ray.position(0).should.equal(point(2, 3, 4))
+        }
+
+        it("position at 1") {
+            ray.position(1).should.equal(point(3, 3, 4))
+        }
+
+        it("position at -1") {
+            ray.position(-1).should.equal(point(1, 3, 4))
+        }
+
+        it("position at 2.5") {
+            ray.position(2.5).should.equal(point(4.5, 3, 4))
         }
     }
 
