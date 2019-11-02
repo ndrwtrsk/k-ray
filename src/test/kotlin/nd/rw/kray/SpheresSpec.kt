2e3082db727a8c0c9d17a5430d2b345993ca0e89
Andrzej Torski <andrew.torski@gmail.com>
2019-11-02 12:41:38 +0100

    refactor spheres test to group intersections tests under one describe

--- src/test/kotlin/nd/rw/kray/SpheresSpec.kt
+++ src/test/kotlin/nd/rw/kray/SpheresSpec.kt
@@ -10,68 +10,70 @@ import org.spekframework.spek2.style.specification.describe
 
 class SpheresSpec : Spek({
 
-    describe("ray intersects a sphere at two points") {
-        val ray = Ray(point(0, 0, -5), vector(0, 0, 1))
-        val sphere = Sphere()
+    describe("intersections"){
+        describe("ray intersects a sphere at two points") {
+            val ray = Ray(point(0, 0, -5), vector(0, 0, 1))
+            val sphere = Sphere()
 
-        val xs = intersect(sphere, ray)
+            val xs = intersect(sphere, ray)
 
-        it("ray should intersect sphere at it's position of 4.0 and 6.0") {
-            xs.size.should.equal(2)
-            xs[0].t.should.equal(4.0)
-            xs[1].t.should.equal(6.0)
-            xs[0].o.should.equal(sphere)
-            xs[1].o.should.equal(sphere)
+            it("ray should intersect sphere at it's position of 4.0 and 6.0") {
+                xs.size.should.equal(2)
+                xs[0].t.should.equal(4.0)
+                xs[1].t.should.equal(6.0)
+                xs[0].o.should.equal(sphere)
+                xs[1].o.should.equal(sphere)
+            }
         }
-    }
 
-    describe("ray intersects a sphere at a tangent") {
-        val ray = Ray(point(0, 1, -5), vector(0, 0, 1))
-        val sphere = Sphere()
+        describe("ray intersects a sphere at a tangent") {
+            val ray = Ray(point(0, 1, -5), vector(0, 0, 1))
+            val sphere = Sphere()
 
-        val xs = intersect(sphere, ray)
+            val xs = intersect(sphere, ray)
 
-        it("ray should intersect sphere at position 5.0") {
-            xs.size.should.equal(2)
-            xs[0].t.should.equal(5.0)
-            xs[1].t.should.equal(5.0)
+            it("ray should intersect sphere at position 5.0") {
+                xs.size.should.equal(2)
+                xs[0].t.should.equal(5.0)
+                xs[1].t.should.equal(5.0)
+            }
         }
-    }
 
-    describe("ray misses a sphere") {
-        val ray = Ray(point(0, 2, -5), vector(0, 0, 1))
-        val sphere = Sphere()
+        describe("ray misses a sphere") {
+            val ray = Ray(point(0, 2, -5), vector(0, 0, 1))
+            val sphere = Sphere()
 
-        val xs = intersect(sphere, ray)
+            val xs = intersect(sphere, ray)
 
-        it("ray has no intersections") {
-            xs.size.should.equal(0)
+            it("ray has no intersections") {
+                xs.size.should.equal(0)
+            }
         }
-    }
 
-    describe("ray originates inside a sphere") {
-        val ray = Ray(point(0, 0, 0), vector(0, 0, 1))
-        val sphere = Sphere()
+        describe("ray originates inside a sphere") {
+            val ray = Ray(point(0, 0, 0), vector(0, 0, 1))
+            val sphere = Sphere()
 
-        val xs = intersect(sphere, ray)
+            val xs = intersect(sphere, ray)
 
-        it("ray should intersect sphere at -1.0 and 1.0") {
-            xs.size.should.equal(2)
-            xs[0].t.should.equal(-1.0)
-            xs[1].t.should.equal(1.0)
+            it("ray should intersect sphere at -1.0 and 1.0") {
+                xs.size.should.equal(2)
+                xs[0].t.should.equal(-1.0)
+                xs[1].t.should.equal(1.0)
+            }
         }
-    }
 
-    describe("sphere is behind a ray") {
-        val ray = Ray(point(0, 0, 5), vector(0, 0, 1))
-        val sphere = Sphere()
+        describe("sphere is behind a ray") {
+            val ray = Ray(point(0, 0, 5), vector(0, 0, 1))
+            val sphere = Sphere()
 
-        val xs = intersect(sphere, ray)
+            val xs = intersect(sphere, ray)
 
-        it("ray should intersect sphere at -6.0 and -4.0") {
-            xs.size.should.equal(2)
-            xs[0].t.should.equal(-6.0)
-            xs[1].t.should.equal(-4.0)
+            it("ray should intersect sphere at -6.0 and -4.0") {
+                xs.size.should.equal(2)
+                xs[0].t.should.equal(-6.0)
+                xs[1].t.should.equal(-4.0)
+            }
         }
     }
 
@@ -91,4 +93,6 @@ class SpheresSpec : Spek({
             sphere.transformation.should.equal(translation(1, 2, 3))
         }
     }
+
+
 })
